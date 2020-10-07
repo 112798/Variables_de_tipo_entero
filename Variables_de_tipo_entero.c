34c8d15b51702f59c7e34af5632bc7f6fa473b14
112798 <71934627+112798@users.noreply.github.com>
2020-10-07 17:11:09 -0500

    Agregando Programas

---
+++ Variables_de_tipo_entero.c
@@ -0,0 +1,18 @@
+//Libreria
+#include <stdio.h>
+
+//Funcion principal
+int main(){
+  /***DECLARACION DE VARIABLES TIPO INTERGER***/
+  //Declaracion de una variable
+  int altura_adulto;
+  altura_adulto=180;
+
+  int altura_infante=140;
+
+  printf("El adulto mide %d cm de altura\n",altura_adulto);
+  printf("El infante mide %d cm de altura\n", altura_infante); 
+
+  printf("Las medidas son: %d (adulto)\t %d (infante)",altura_adulto,altura_infante);
+
+}
